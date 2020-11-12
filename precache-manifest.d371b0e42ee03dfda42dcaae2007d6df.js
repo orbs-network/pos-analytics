631c493a9258327fcb230f208cd585ce53b782d6
denis6421 <denisn@mixin.co.il>
2020-11-12 17:26:22 +0200

    Updates

---
+++ precache-manifest.d371b0e42ee03dfda42dcaae2007d6df.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c274d4bd87cfffcd68888fad55e035",
+    "url": "/pos-analytics/index.html"
+  },
+  {
+    "revision": "be3e2d710e2737487751",
+    "url": "/pos-analytics/static/css/main.7491d4ca.chunk.css"
+  },
+  {
+    "revision": "1243ab529bce12a305b9",
+    "url": "/pos-analytics/static/js/2.e88e079f.chunk.js"
+  },
+  {
+    "revision": "2ff746e50630b8256156c4fd1a10cfbe",
+    "url": "/pos-analytics/static/js/2.e88e079f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be3e2d710e2737487751",
+    "url": "/pos-analytics/static/js/main.35e8bf82.chunk.js"
+  },
+  {
+    "revision": "f72f59e290725314793e",
+    "url": "/pos-analytics/static/js/runtime-main.d545899c.js"
+  },
+  {
+    "revision": "c698f8c22a6f64f6c86f310dd2b42a88",
+    "url": "/pos-analytics/static/media/coins.c698f8c2.svg"
+  },
+  {
+    "revision": "5ae6ccfeb22ce53c5aaaf6e4ffe00379",
+    "url": "/pos-analytics/static/media/copy.5ae6ccfe.svg"
+  },
+  {
+    "revision": "3ee6411fe1d1c0d8e013b07249a8af90",
+    "url": "/pos-analytics/static/media/cpu.3ee6411f.svg"
+  },
+  {
+    "revision": "fe49925e8fb7b2d8c257370e05f4e751",
+    "url": "/pos-analytics/static/media/delegators-selected.fe49925e.svg"
+  },
+  {
+    "revision": "091557cd043a610188ed9b9df9957f3b",
+    "url": "/pos-analytics/static/media/delegators.091557cd.svg"
+  },
+  {
+    "revision": "fc6fbf165a4e75d50d06943c982bc83b",
+    "url": "/pos-analytics/static/media/encrypted.fc6fbf16.svg"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/pos-analytics/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "e200a932fc096c998589d021de81b3bd",
+    "url": "/pos-analytics/static/media/guardians-selected.e200a932.svg"
+  },
+  {
+    "revision": "95190aa2983a1102d306e06ec1bdf595",
+    "url": "/pos-analytics/static/media/guardians.95190aa2.svg"
+  },
+  {
+    "revision": "180bed75a431a4acc7d7860a93f14aac",
+    "url": "/pos-analytics/static/media/japan.180bed75.svg"
+  },
+  {
+    "revision": "bf799dad96e9305a90808512c90f65e2",
+    "url": "/pos-analytics/static/media/logo orbs.bf799dad.svg"
+  },
+  {
+    "revision": "ecef733a33ceec8dc32d29510998afe9",
+    "url": "/pos-analytics/static/media/loupe.ecef733a.svg"
+  },
+  {
+    "revision": "863932b67840a784f8e99543ac82d9d9",
+    "url": "/pos-analytics/static/media/navbar-logo.863932b6.svg"
+  },
+  {
+    "revision": "57aad41c36a29cb5ce924741031f6e57",
+    "url": "/pos-analytics/static/media/overview-selected.57aad41c.svg"
+  },
+  {
+    "revision": "416db91d239c91b5446bfc32e77b532c",
+    "url": "/pos-analytics/static/media/overview.416db91d.svg"
+  },
+  {
+    "revision": "ad512fa5b533415815b16b5631654929",
+    "url": "/pos-analytics/static/media/south-korea.ad512fa5.svg"
+  },
+  {
+    "revision": "7cf558ab81b6649b19f1a265cf4030b7",
+    "url": "/pos-analytics/static/media/stellar.7cf558ab.svg"
+  },
+  {
+    "revision": "51d4bc2c748eeacf8ae71aea87c918dc",
+    "url": "/pos-analytics/static/media/token.51d4bc2c.png"
+  },
+  {
+    "revision": "4cda8b81115fe413d2aeac2d41f8f7e2",
+    "url": "/pos-analytics/static/media/trophy.4cda8b81.svg"
+  },
+  {
+    "revision": "a035edadeadb79c859f4a31c4392b1bb",
+    "url": "/pos-analytics/static/media/usa.a035edad.svg"
+  },
+  {
+    "revision": "bac02ede5c8574264a8ec15c6e504b6d",
+    "url": "/pos-analytics/static/media/voting-booth.bac02ede.svg"
+  }
+]);
