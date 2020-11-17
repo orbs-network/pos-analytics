c472823b2247d54d25ea583d80c6d90eb6154db8
Noam Berg <noamberg@yahoo.com>
2020-11-17 10:55:14 +0200

    change node endpoint to new guardian

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -4,7 +4,7 @@ import { SupportedLanguage } from '../../global/types';
 import { LOCAIZE_API, LOCAIZE_PROJECT_ID } from '../../global/variables';
 class Api {
     ethereumEndpoint = 'https://mainnet.infura.io/v3/9679dc4f2d724f7997547f05f769d74e';
-    nodeEndpoints = ['https://guardian.v2beta.orbs.com/services/management-service/status'];
+    nodeEndpoints = ['https://0xcore.orbs.com/services/management-service/status'];
     async getDelegatorApi(address: string) {
         try {
             const res = await getDelegator(address, this.ethereumEndpoint);
