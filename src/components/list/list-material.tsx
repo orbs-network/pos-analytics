6cdb239fd6c4c955c5a5747be439ae4cd25959f1
razmixin <razmixin.co.il>
2020-12-09 13:25:27 +0200

    guardian-delegator list

--- src/components/list/list-material.tsx
+++ src/components/list/list-material.tsx
@@ -10,41 +10,45 @@ import Paper from '@material-ui/core/Paper';
 import './list.scss';
 
 interface StateProps {
-    titles: string[];
-    titleClassName: string;
-    children: any;
-    listClassName?: string;
-    listHeaderBg?: string;
+  titles: string[];
+  titleClassName: string;
+  children: any;
+  listClassName?: string;
+  listHeaderBg?: string;
 }
 
 export const ListMaterial = (props: StateProps) => {
-    const { titles, titleClassName, listClassName, listHeaderBg } = props;
-    const className = listClassName ? `list ${listClassName}` : 'list';
-    return (
-        <TableContainer
-            component={Paper}
-            className={className}
-            style={{
-                boxShadow: 'none'
-            }}>
-            <Table  stickyHeader>
-                <TableHead>
-                    <TableRow>
-                        {titles.map((title: string, index: number) => {
-                            return (
-                                <TableCell
-                                    key={index}
-                                    style={{
-                                        background: listHeaderBg || 'transparent'
-                                    }}>
-                                    <p className={`${titleClassName} one-line capitalize`}>{title}</p>
-                                </TableCell>
-                            );
-                        })}
-                    </TableRow>
-                </TableHead>
-                <TableBody>{props.children}</TableBody>
-            </Table>
-        </TableContainer>
-    );
+  const { titles, titleClassName, listClassName, listHeaderBg } = props;
+  const className = listClassName ? `list ${listClassName}` : 'list';
+  return (
+    <TableContainer
+      component={Paper}
+      className={className}
+      style={{
+        boxShadow: 'none',
+      }}
+    >
+      <Table stickyHeader>
+        <TableHead>
+          <TableRow>
+            {titles.map((title: string, index: number) => {
+              return (
+                <TableCell
+                  key={index}
+                  style={{
+                    background: listHeaderBg || 'transparent',
+                  }}
+                >
+                  <p className={`${titleClassName} one-line capitalize`}>
+                    {title}
+                  </p>
+                </TableCell>
+              );
+            })}
+          </TableRow>
+        </TableHead>
+        <TableBody>{props.children}</TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
--- src/screens/guardians/sections/guardian-delegators/components/guardian-delegator/guardian-delegator.tsx
+++ src/screens/guardians/sections/guardian-delegators/components/guardian-delegator/guardian-delegator.tsx
@@ -7,28 +7,28 @@ import { GuardianDelegator } from '@orbs-network/pos-analytics-lib';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 interface StateProps {
-    delegator: GuardianDelegator;
+  delegator: GuardianDelegator;
 }
 
 export const GuardianDelegatorElement = ({ delegator }: StateProps) => {
-    const { address, stake, non_stake } = delegator;
-    return (
-        <TableRow>
-            <TableCell align="left"
-            >
-                <Link
-                    className="list-item flex-start-center"
-                    to={routes.delegators.stake.replace(':address', address)}>
-                    <p> {address}</p>
-                    <img src={CopyImg} alt="" />
-                </Link>
-            </TableCell>
-            <TableCell align="left">
-                <p className="list-item">{convertToString(stake)}</p>
-            </TableCell>
-            <TableCell align="left">
-                <p className="list-item">{convertToString(non_stake)}</p>
-            </TableCell>
-        </TableRow>
-    );
+  const { address, stake, non_stake } = delegator;
+  return (
+    <TableRow>
+      <TableCell align="left">
+        <Link
+          className="list-item flex-start-center"
+          to={routes.delegators.stake.replace(':address', address)}
+        >
+          <p> {address}</p>
+          <img src={CopyImg} alt="" />
+        </Link>
+      </TableCell>
+      <TableCell align="left">
+        <p className="list-item">{convertToString(stake)}</p>
+      </TableCell>
+      <TableCell align="left">
+        <p className="list-item">{convertToString(non_stake)}</p>
+      </TableCell>
+    </TableRow>
+  );
 };
