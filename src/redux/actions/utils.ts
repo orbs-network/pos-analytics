f9419b9e68a0be5b6b87741064422f832d21a9d7
denis-orbs <denis@orbs.com>
2024-07-31 10:36:22 +0300

    fixed guardian page

--- src/redux/actions/utils.ts
+++ src/redux/actions/utils.ts
@@ -3,16 +3,17 @@ import Cookies from 'universal-cookie';
 export async function getRefBlock(web3: any, oldestActionBlock: number) {
     const chainId = await web3.eth.getChainId();
     const cookies = new Cookies();
-    const cachedData = cookies.get(`refBlock_${chainId}`);
+    const cachedData = cookies.get(`refBlock_${chainId}`);    
     const refBlock =
         cachedData && cachedData.block < oldestActionBlock
             ? cachedData
-            : { number: oldestActionBlock, time: (await web3.eth.getBlock(oldestActionBlock)).timestamp };
+            : { number: oldestActionBlock, time: (await web3.eth.getBlock(oldestActionBlock))?.timestamp };
     cookies.set(`refBlock_${chainId}`, refBlock);
     return refBlock;
 }
 
 export async function getAvgBlockTime(web3: any, refBlock: any): Promise<number> {
     const latestBlock = await web3.eth.getBlock('latest');
+    
     return (latestBlock.timestamp - refBlock.time) / (latestBlock.number - refBlock.number);
 }
