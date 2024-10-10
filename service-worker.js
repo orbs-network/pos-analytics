a155a1a7cc3aa16caab54941b3939562d18c74e9
idanatar <idana@orbs.com>
2024-10-10 12:35:59 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c99020916a0e50e23eb20d7f15d104e.js"
+  "/precache-manifest.4d33d3d2f51788f2efd5a0a71a85b777.js"
 );
 
 self.addEventListener('message', (event) => {
